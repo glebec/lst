13b4dbe6060f332c360d726d33be71dac371a671
Gabriel Lebec <glebec@gmail.com>
2015-07-15 17:15:27 -0400

    basic cli parsing

--- bin/lst.js
+++ bin/lst.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
 'use strict';
+var program = require('commander');
+var chalk = require('chalk');
+var path = require('path');
+
+function coerceLevel (n) {
+  n = parseInt(n, 10);
+  return n > 0 ? n : 1;
+}
+
+program
+  .version('1.0.0')
+  .description('Recursively print a pretty directory tree')
+  .option('-m, --node_modules', 'recurse through node_modules')
+  .option('-l, --level <n>', 'recurse <n> levels deep', coerceLevel)
+  .arguments('[dir]')
+  .action(function (dir) {
+     program.dir = path.resolve(dir);
+  })
+  .parse(process.argv);
+
+program.dir = program.dir || path.resolve(process.cwd());
